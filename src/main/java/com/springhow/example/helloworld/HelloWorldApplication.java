4e3b579b8bc771b9c1d9a02505491ec267814f7b
arsadak <89344137+arsadak@users.noreply.github.com>
2022-07-05 22:49:30 +0800

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "5. i have successfully deployed the project on production environment";
     }
 
 }
